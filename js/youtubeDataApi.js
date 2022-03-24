ecd4bb512bc723c9b123696ff1794772656e8c4f
Tenjin Kageyu <hkageyu@gftd.works>
2022-03-24 14:44:21 +0900

    変更

--- js/youtubeDataApi.js
+++ js/youtubeDataApi.js
@@ -2,36 +2,46 @@
 //
 //
 
-var menuOnOff = false;
+// メニューオン・オフ時の CSS の display
+const MENU_ON = "1";
+const MENU_OFF = "0";
+// js 内ので保持
+var menuOnOff = MENU_OFF;
 
 function clickBtn(button = null) {
     if (button == null) {
-
-        menuOnOff = !menuOnOff;
         var navToggle = document.getElementById("nav-toggle");
-        if (menuOnOff) {
+        if (menuOnOff == MENU_OFF) {
+            menuOnOff = MENU_ON;
             // オンになったら
             navToggle.innerHTML = "◯";
-            navToggle.display = 'inline';
+            //navToggle.display = MENU_ON;
         }
         else {
+            menuOnOff = MENU_OFF;
             // オフになったら
             navToggle.innerHTML = "●";
-            navToggle.display = 'none';
+            //navToggle.display = MENU_OFF;
         }
         var navList = document.getElementById("nav-list");
-        var navi = navList.getElementsByClassName("navi");
+/*
+        var navi = navList.getElementsByClassName("nav");
         for (var key in navi) {
-            console.log(navi[key]);
-            navi[key].display = navToggle.display;
+            if (navi[key] && navi[key].style) { // ←解決する演算子.?的なのがあったはず
+                navi[key].style.opacity = menuOnOff;
+                console.log("opacity=" + navi[key].style.opacity);
+            }
         }
+ */
+        var opa = navList.getElementsByClassName("opacity");
+        console.log("opa=" + opa);
     }
-    else {
-        const elements = document.getElementsByClassName("navi");
-        Array.prototype.forEach.call(elements, function (e1) {
-            const e2 = e1.
-            console.log(e1);
-        });
-        console.log(button);
-    }
+    // else {
+    //     const elements = document.getElementsByClassName("navi");
+    //     Array.prototype.forEach.call(elements, function (e1) {
+    //         const e2 = e1.
+    //         console.log(e1);
+    //     });
+    //     console.log(button);
+    // }
 }
